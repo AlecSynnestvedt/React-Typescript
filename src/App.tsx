09bbe8e245c303d322e6ee72d44b40982bb8a399
Alec Synnestvedt <aleccsyn@gmail.com>
2021-05-14 15:46:27 -0400

    sync

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+import Clock from './components/Clock';
 import './App.css';
 
-function App() {
+let testProp: string = 'Am I getting passed to the clock component?'
+let optionalProp: string = 'You sure are!'
+
+const App: React.FunctionComponent = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className="verticalCenter">
+        <Clock testProp={testProp} optionalProp={optionalProp}/>
+     </div>
     </div>
   );
 }
